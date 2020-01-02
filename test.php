1548b01602a8f28382173edf5b14f446d39d9578
Muhammad Ikbal <44798071+mikbalvia@users.noreply.github.com>
2020-01-02 19:25:06 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,150 +1,70 @@
 <?php
+$datapemain = [
+    [ 'pemain'=> 'A', 'nilai'=> [], 'tambahan'=> [] ],
+    [ 'pemain'=> 'B', 'nilai'=> [], 'tambahan'=> [] ],
+    [ 'pemain'=> 'C', 'nilai'=> [], 'tambahan'=> [] ],
+    [ 'pemain'=> 'D', 'nilai'=> [], 'tambahan'=> [] ]
+];
 
-class Dadu {
-   
-    private $valSisiAtas;
-    public function getvalSisiAtas()
-    {
-        return $this->valSisiAtas;
-    }
-    public function rolling()
-    {
-        $this->valSisiAtas =  rand(1,6);
-        return $this;
-    }
-    public function setvalSisiAtas($valSisiAtas)
-    {
-        $this->valSisiAtas = $valSisiAtas;
-        return $this;
-    }
+
+
+function randomNilai($jumlahLempar,$key){
+    global $datapemain;
+    for ($i=0; $i <$jumlahLempar ; $i++) { 
+        array_push($datapemain[$key]['nilai'],rand(1,6));
+    }  
 }
-class pemain {
-    private $daduDalamCup = array();
-    private $nama;
-    private $posisi;
-    public function getdaduDalamCup()
-    {
-        return $this->daduDalamCup;
-    }
-    public function getnama()
-    {
-        return $this->nama;
-    }
-    public function getposisi()
-    {
-        return $this->posisi;
-    }
-    public function __construct($nomorDadu,$posisi,$nama='')
-    {
-        $this->posisi = $posisi;
-        $this->nama = $nama;
-        for($i=0;$i<$nomorDadu;$i++){
-            array_push($this->daduDalamCup,new Dadu());
+
+function removeNilai($key){
+    global $datapemain;
+    for ($i=0; $i <count($datapemain[$key]['nilai']) ; $i++) { 
+        if ($datapemain[$key]['nilai'][$i]==6) {
+            array_splice($datapemain[$key]['nilai'],$i,1);
+            $i--;
+        }elseif ($datapemain[$key]['nilai'][$i]==1) {
+            array_splice($datapemain[$key]['nilai'],$i,1);
+            $i--;
+            if ($key===3) {
+                array_push($datapemain[0]['tambahan'],1);
+            }else{
+                array_push($datapemain[$key+1]['tambahan'],1);
+            }
         }
     }
-    public function play()
-    {
-        foreach($this->daduDalamCup as $dadu){
-            $dadu->rolling();
-        }
-    }
-    public function removedadu($key)
-    {
-       
-        unset($this->daduDalamCup[$key]);
-    }
-    public function insertdadu($dadu)
-    {
-        array_push($this->daduDalamCup,$dadu);
-    }
+
 }
-class Game{
-    /** @var pemain[] $pemains */
-    private $pemains = array();
-    private $round;
-    const NOMOR_DADU_TIAP_PEMAIN = 6;
-    const NOMOR_PEMAIN = 4;
-    const HAPUS_DADU = 6;
-    const PINDAH_DADU = 1;
-    public function __construct()
-    {
-        $this->round = 0;
-        for($i=0;$i<self::NOMOR_PEMAIN;$i++){
-            $this->pemains[$i] = new pemain(self::NOMOR_DADU_TIAP_PEMAIN,$i,chr(65+$i));
+
+$jumlahLempar = 6;
+$cekAnggota = true;
+$round = 1;
+while ($cekAnggota==true) {
+    echo '<br><br>Round '.$round.'<br><br>After dice rolled:<br>';
+    foreach ($datapemain as $key=>$value) {
+        if (count($value['nilai'])!=0) {
+            $jumlahLempar=count($value['nilai']);
         }
+        $datapemain[$key]['nilai']=[];
+        $datapemain[$key]['tambahan']=[];
+        randomNilai($jumlahLempar,$key);
+        echo 'Player '.$value['pemain'].': '.implode(',',$datapemain[$key]['nilai']).'<br>';
     }
-    public function Rounde()
-    {
-        echo "<strong>Round ".$this->round."</strong><br><br>\r\n";
-        return $this;
+    foreach ($datapemain as $key=>$value) {
+        removeNilai($key);
     }
-    public function tampilSisiAtasDadu($title='After dice rolled')
-    {
-        echo '<span style="text-decoration: underline;">'.$title.'</span><br>';
-        foreach($this->pemains as $pemain){
-            echo "Player ".$pemain->getnama().": ";
-            $dadaSisiAtas = '';
-            foreach($pemain->getdaduDalamCup() as $dadu){
-                $dadaSisiAtas .= $dadu->getvalSisiAtas().", ";
-            }
-            echo rtrim($dadaSisiAtas,",")."<br>\r\n";
-        }
-        echo "<br><br>\r\n";
-        return $this;
+    echo '<br>After dice moved/removed:<br>';
+
+    foreach ($datapemain as $key=>$value) {
+        $datapemain[$key]['nilai']=array_merge($datapemain[$key]['nilai'],$datapemain[$key]['tambahan']);
+        echo 'Player '.$value['pemain'].': '.implode(',',$datapemain[$key]['nilai']).'<br>';
     }
-    public function tampilWinner($pemain)
-    {
-        echo "<h3>Found Winner</h3>\r\n";
-        echo "Player ".$pemain->getnama()."<br>\r\n";
-        return $this;
+
+    foreach ($datapemain as $key=>$value) {
+        if (count($datapemain[$key]['nilai'])==0) {
+            $cekAnggota=false;
+            echo '<br>++++++<br><h2>Player '.$value['pemain'].' won</h2>';
+        } 
     }
-    public function start()
-    {
-        while(true){
-            $this->round++;
-            $daduNext = array();
-            foreach($this->pemains as $pemain){
-                $pemain->play();
-            }
-            $this->Rounde()->tampilSisiAtasDadu();
-            foreach($this->pemains as $index=>$pemain){
-                $daduArray = array();
-                foreach($pemain->getdaduDalamCup() as $daduIndex=>$dadu){
-                    if($dadu->getvalSisiAtas() == self::HAPUS_DADU){
-                        $pemain->removedadu($daduIndex);
-                    }
-                    if($dadu->getvalSisiAtas() == self::PINDAH_DADU){
-                        if($pemain->getposisi()==(self::NOMOR_PEMAIN-1)){
-                            $this->pemains[0]->insertdadu($dadu);
-                            $pemain->removedadu($daduIndex);
-                        }
-                        else{
-                            array_push($daduArray,$dadu);
-                            $pemain->removedadu($daduIndex);
-                        }
-                    }
-                }
-                $daduNext[$index+1] =$daduArray;
-                if(array_key_exists($index,$daduNext) && count($daduNext[$index])>0){
-                    foreach ($daduNext[$index] as $dadu) {
-                        $pemain->insertdadu($dadu);
-                    }
-                    $daduNext = array();
-                }
-            }
-            $this->tampilSisiAtasDadu("After dice moved/removed");
-            $hitungPemenang=0;
-            foreach($this->pemains as $pemain){
-                if(count($pemain->getdaduDalamCup())<=0){
-                    $this->tampilWinner($pemain);
-                    $hitungPemenang++;
-                }
-            }
-            if($hitungPemenang>0){
-                break;
-            }
-        }
-    }
-}
-$game = new Game();
-$game->start();
+    
+    $round++;
+};
+?>
